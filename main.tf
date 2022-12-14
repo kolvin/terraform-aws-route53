2791cc72a9cedbb95cb839ab85330838356ca70c
kolvin <15124052+Kolvin@users.noreply.github.com>
2022-12-14 21:25:26 +0000

    fix: lint
    Signed-off-by: kolvin <15124052+Kolvin@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_route53_zone" "route53_zone" {
   for_each = local.route53_zones
 
-  name              = each.value.name
-  comment           = each.value.type == "public" ? "${each.value.name} public zone" : "${each.value.name} private zone"
-  force_destroy     = each.value.force_destroy
+  name          = each.value.name
+  comment       = each.value.type == "public" ? "${each.value.name} public zone" : "${each.value.name} private zone"
+  force_destroy = each.value.force_destroy
 
   dynamic "vpc" {
     for_each = each.value.type == "private" ? [each.value] : []
--- vars.tf
+++ vars.tf
@@ -1,10 +1,10 @@
 variable "route53_zones" {
   type = list(object({
-    name              = string
-    type              = string
-    force_destroy     = bool
-    vpc_id            = string
-    tags              = map(string)
+    name          = string
+    type          = string
+    force_destroy = bool
+    vpc_id        = string
+    tags          = map(string)
     dns_records = list(object({
       domain_name = string
       record_type = string
