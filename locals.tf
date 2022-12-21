9bb891583f2b3338cfeef39ee271de08248f6390
kolvin <15124052+Kolvin@users.noreply.github.com>
2022-12-21 20:08:12 +0000

    feat(route53): add resoource block to support creating route53 hosted zone dns records.
    loop over hosted zone inputs and build unnique collection of records for zone
    
    Signed-off-by: kolvin <15124052+Kolvin@users.noreply.github.com>

--- locals.tf
+++ locals.tf
@@ -1,3 +1,13 @@
 locals {
   route53_zones = { for zone in var.route53_zones : "${zone.name}-${zone.type}" => zone }
+  route53_zone_records = {
+    for record in flatten(
+      [
+        for zone in var.route53_zones :
+        [
+          for dns_record in zone.dns_records : merge({ "zone_record_name" = "${zone.name}-${zone.type}" }, dns_record)
+        ]
+      ]
+    ) : "${record.domain_name}_${record.record_type}" => record
+  }
 }
